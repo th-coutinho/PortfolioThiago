699c9daee461f2c524e997541c680fb73ff67b2f
udilia <thiago.coutinho@al.forsoft.org.br>
2017-09-28 13:59:49 -0300

    minified version v.1.0.0

--- js/portfolio-animations.js
+++ js/portfolio-animations.js
@@ -1,22 +1,77 @@
- var teste;   
+ var buttonState = false;   
 
    $(document).ready(function(){
       $(window).scroll(function(){
          //navAnimation();
          brandAnimation();
-      });
+      })
+   });
+
+   $(".navbar-button").click( function() {
+      $(".navigationBar").slideToggle("slow");  
+   });
+
+   $(".contact-me").click( function() {
+      changeContactButton();      
    });
 
    $(".nav-button").click( function() {
-      //$('body').scrollTo(".skills-title");
-      //$(".extra").animatedScroll({duration: 1000, easing: "easeOutExpo"}, {top: "60%"});
       var divId = $(this).attr("id");
-      jumpToDiv(divId);
-      //alert(divId);
+      var adjust = 0;
+
+      if (divId == 'welcome') {
+         adjust = 850;
+      }
+
+      if (divId == 'skills') {
+         adjust = 20;
+      }
+
+      if (divId == 'extra') {
+         adjust = 80;
+      }
+
+      if (divId == 'exp') {
+         adjust = 80;
+      }
+
+      if (divId == 'tools') {
+         adjust = 30;
+      }
+
+      if (divId == 'contact') {
+         adjust = 30;
+      }
+
+
+      jumpToDiv(divId, adjust);
    });
 
-   function jumpToDiv(name, position) {
-      $(name).animatedScroll({duration: 1000, easing: "easeOutExpo"}, {top: "60%"});
+   $(".nav-button")
+
+          .mouseover(function(){
+             var name = $(this).attr('id');
+              name = "#line-" + name;
+              //alert(name);
+              $(name).addClass("nav-itens-line-marked");
+          })
+
+
+          .mouseout(function(){
+             var name = $(this).attr('id');
+              name = "#line-" + name;
+             // alert(name);
+              $(name).removeClass("nav-itens-line-marked");
+   });   
+
+   function jumpToDiv(name, adjust) {
+      name = "." + name;
+
+      $(name).animatescroll({scrollSpeed:1000, padding:adjust, easing:'easeInOutQuad'});
+
+      if ($(window).width() < 768) {
+         $(".navigationBar").slideUp("slow");
+      }
    }
 
    function navAnimation() {
@@ -32,6 +87,25 @@
             "background": previouslyColor
          });
       }
+   }
+
+   function changeContactButton () { 
+      if (buttonState == false) {
+         $(".contact-me").addClass("pressed-button", function(){
+            buttonState = true;
+            var buttonHtml = "<p class='button-email'>Thiagoscoutinho92@gmail.com</p>" + "<hr class='button-hr'>" + "<p class='button-tel'>21 9 8095-3323</p>";
+            changeButtonText(buttonHtml);
+         });
+      } else {
+         $(".contact-me").removeClass("pressed-button", function(){
+            buttonState = false;
+            changeButtonText("ENTRE EM CONTATO");
+         });
+      }      
+   }
+
+   function changeButtonText(text) {
+      $(".contact-me").html(text);
    }
 
    function brandAnimation() {
